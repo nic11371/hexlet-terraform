866925e666f617757fb6d2c405e1194da9d265ae
Nikolay Melnikov <nic11371@gmail.com>
2025-04-15 12:23:15 +0300

    create cloud infrastructure

---
+++ secret_db.auto.tfvars
@@ -0,0 +1,3 @@
+db_name = "hexlet"
+db_user = "me"
+db_password = "bvcdV6sdBS7AXZs"
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "yc_iam_token" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
+
+variable "yc_cloud_id" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
+
+variable "yc_folder_id" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
+
+variable "db_user" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
+
+variable "db_password" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
+
+variable "db_name" {
+    description = ""
+    type = string
+    default = ""
+    sensitive = true
+}
