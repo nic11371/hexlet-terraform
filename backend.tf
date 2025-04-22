4481c8ccf90ff4f44ee55e192f5fa797c2d719c7
Nikolay Melnikov <nic11371@gmail.com>
2025-04-22 08:57:57 +0300

    update config

--- backend.tf
+++ backend.tf
@@ -1,11 +1,16 @@
 terraform {
   backend "s3" {
-    endpoint                    = "https://storage.yandexcloud.net"
-    region                      = "ru-central1"
-    key                         = "hexlet-remote-state"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "yc-hexlet-states"
+    region = "ru-central1"
+    key    = "hexlet-remote-state"
+
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+
   }
 }
